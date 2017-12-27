1dd696453d3704fefba1b48e17ae0259cc62a90c
yejinyun <ye6478303>
2017-12-27 16:20:31 +0800

    构建一些虚拟的数据

--- app/src/main/java/com/yejy/wealthcatch/ui/fragment/HomeFragment.java
+++ app/src/main/java/com/yejy/wealthcatch/ui/fragment/HomeFragment.java
@@ -5,15 +5,20 @@ import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.view.LayoutInflater;
 import android.view.View;
+import android.widget.BaseAdapter;
+import android.widget.ListView;
 import android.widget.TextView;
 
 import com.yejy.wealthcatch.R;
 import com.yejy.wealthcatch.ui.base.BaseFragment;
+import com.yejy.wealthcatch.ui.fragment.adapter.NewMessageAdapter;
 
 
 public class HomeFragment extends BaseFragment {
     TextView topTitle;
     private static final String TITLE_KEY = "title_key";
+    private ListView listView;
+    private BaseAdapter baseAdapter;
 
     public static HomeFragment newInstance(String title) {
         HomeFragment homeFragment = new HomeFragment();
@@ -27,6 +32,7 @@ public class HomeFragment extends BaseFragment {
     protected View onCreateView() {
         View contentView = LayoutInflater.from(getActivity()).inflate(R.layout.frag_home, null);
         topTitle = contentView.findViewById(R.id.topTitle);
+        listView = contentView.findViewById(R.id.listView);
         return contentView;
     }
 
@@ -35,5 +41,9 @@ public class HomeFragment extends BaseFragment {
         super.onViewCreated(view, savedInstanceState);
         String title = getArguments().getString(TITLE_KEY, "");
         topTitle.setText(title);
+        if (title.equals("彩票资讯")) {
+            baseAdapter = new NewMessageAdapter();
+        }
+        listView.setAdapter(baseAdapter);
     }
 }
