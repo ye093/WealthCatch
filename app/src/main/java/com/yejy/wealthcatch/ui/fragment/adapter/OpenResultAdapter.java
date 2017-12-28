aa129b0cbf79fcd71524362210fd85c41745848b
yejy <ye_093@163.com>
2017-12-28 23:56:23 +0800

    第二次提交……………………

--- app/src/main/java/com/yejy/wealthcatch/ui/fragment/adapter/OpenResultAdapter.java
+++ app/src/main/java/com/yejy/wealthcatch/ui/fragment/adapter/OpenResultAdapter.java
@@ -6,6 +6,9 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageView;
 
+import com.qmuiteam.qmui.widget.dialog.QMUIDialog;
+import com.qmuiteam.qmui.widget.dialog.QMUIDialogAction;
+import com.qmuiteam.qmui.widget.dialog.QMUITipDialog;
 import com.yejy.wealthcatch.R;
 
 /**
@@ -14,10 +17,23 @@ import com.yejy.wealthcatch.R;
 
 public class OpenResultAdapter extends RecyclerView.Adapter<OpenResultAdapter.Holder> {
     private static final int[] RES_ID = new int[] {
-            R.drawable.icon_launch,
-            R.drawable.icon_launch,
-            R.drawable.icon_launch,
-            R.drawable.icon_launch
+            R.drawable.daletou,
+            R.drawable.fucai3d,
+            R.drawable.pailie3,
+            R.drawable.shuangseqiu
+    };
+    private final static String[] TITLES = new String[]{
+            "大乐透",
+            "3D福彩",
+            "排列3",
+            "双色球"
+    };
+
+    private final static String[] NUMBERS = new String[]{
+            "23 07 28 02 04 | 02 08",
+            "0 3 1",
+            "3 8 4",
+            "07 11 12 13 18 19 | 16"
     };
 
     @Override
@@ -28,8 +44,17 @@ public class OpenResultAdapter extends RecyclerView.Adapter<OpenResultAdapter.Ho
     }
 
     @Override
-    public void onBindViewHolder(Holder holder, int position) {
+    public void onBindViewHolder(Holder holder, final int position) {
         holder.imageView.setImageResource(RES_ID[position]);
+        holder.imageView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                QMUIDialog.MessageDialogBuilder messageDialogBuilder = new QMUIDialog.MessageDialogBuilder(view.getContext());
+                messageDialogBuilder.setTitle(TITLES[position] + "开奖结果")
+                        .setMessage(NUMBERS[position]);
+                messageDialogBuilder.create().show();
+            }
+        });
     }
 
     @Override
@@ -42,7 +67,7 @@ public class OpenResultAdapter extends RecyclerView.Adapter<OpenResultAdapter.Ho
 
         public Holder(View itemView) {
             super(itemView);
-            imageView = (ImageView) itemView;
+            imageView = itemView.findViewById(R.id.image_view);
         }
     }
 }
